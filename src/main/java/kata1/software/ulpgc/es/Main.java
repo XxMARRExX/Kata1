5b403082b3e888afaae4533291c4ad231d80d0f7
XxMARRExX <xx_marre_xx@hotmail.com>
2024-06-26 12:15:04 +0100

    Implementation of Player and tests

--- src/main/java/kata1/software/ulpgc/es/Main.java
+++ src/main/java/kata1/software/ulpgc/es/Main.java
@@ -5,5 +5,13 @@ import model.kata1.software.ulpgc.es.*;
 public class Main {
     public static void main(String[] args) {
 
+        Player player1 = new Player("Jude Bellingham" , 19, "Midfielder");
+        Player player2 = new Player("Vinicius Junior" , 23, "Striker");
+        Player player3 = new Player("Thibaut Courtois" , 29, "Goalkeeper");
+
+        System.out.print(player1.toString());
+        System.out.print(player2.toString());
+        System.out.print(player3.toString());
+
     }
 }
--- src/main/java/model/kata1/software/ulpgc/es/Player.java
+++ src/main/java/model/kata1/software/ulpgc/es/Player.java
@@ -1,4 +1,30 @@
 package model.kata1.software.ulpgc.es;
 
-public class Player {
+public class Player extends Person{
+
+    private int age;
+    private String position;
+
+    public Player(String name, int age, String position) {
+        super(name);
+        this.age = age;
+        this.position = position;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                " Name: " + super.getName() +
+                ", age: " + getAge() +
+                ", position='" + getPosition() + '\'' +
+                " }\n";
+    }
 }
