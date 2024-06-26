6997c226b7121d91f92356782155b9d3f682efb2
XxMARRExX <xx_marre_xx@hotmail.com>
2024-06-26 12:19:11 +0100

    Implementation of Coach and tests

--- src/main/java/kata1/software/ulpgc/es/Main.java
+++ src/main/java/kata1/software/ulpgc/es/Main.java
@@ -5,9 +5,13 @@ import model.kata1.software.ulpgc.es.*;
 public class Main {
     public static void main(String[] args) {
 
+        Coach coach = new Coach("Carlo Anchelotti" , 27);
+
         Player player1 = new Player("Jude Bellingham" , 19, "Midfielder");
         Player player2 = new Player("Vinicius Junior" , 23, "Striker");
         Player player3 = new Player("Thibaut Courtois" , 29, "Goalkeeper");
+
+        System.out.println(coach.toString());
 
         System.out.print(player1.toString());
         System.out.print(player2.toString());
--- src/main/java/model/kata1/software/ulpgc/es/Coach.java
+++ src/main/java/model/kata1/software/ulpgc/es/Coach.java
@@ -1,4 +1,23 @@
 package model.kata1.software.ulpgc.es;
 
-public class Coach {
+public class Coach extends Person{
+
+    private int experienceYears;
+
+    public Coach(String name, int experienceYears) {
+        super(name);
+        this.experienceYears = experienceYears;
+    }
+
+    public int getExperienceYears() {
+        return experienceYears;
+    }
+
+    @Override
+    public String toString() {
+        return "Coach{" +
+                " Name: " + getName() +
+                ", experienceYears: " + experienceYears +
+                " }";
+    }
 }
