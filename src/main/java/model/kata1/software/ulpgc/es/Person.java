ac1a22b57243a4510d0057e8fcec51c946d22872
XxMARRExX <xx_marre_xx@hotmail.com>
2024-06-26 12:07:19 +0100

    Implementation of Class Person

--- src/main/java/model/kata1/software/ulpgc/es/Person.java
+++ src/main/java/model/kata1/software/ulpgc/es/Person.java
@@ -1,4 +1,14 @@
 package model.kata1.software.ulpgc.es;
 
 public abstract class Person {
+
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
